4a06821b3c5871fc6e6fcb69a34d9df04d049305
Willbemax <max22042007@yandex.ru>
2024-03-20 21:48:57 +0300

    Работа надо ошибкой активации аккаунта

---
+++ client/src/components/LoginForm.tsx
@@ -0,0 +1,21 @@
+import { FC, useContext, useState } from "react"
+import { Context } from "../main"
+
+
+const LoginForm: FC = () => {
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const { store } = useContext(Context)
+
+
+    return (
+        <div>
+            <input type="email" placeholder="email" onChange={e => setEmail(e.target.value)} value={email} />
+            <input type="email" placeholder="password" onChange={e => setPassword(e.target.value)} value={password} />
+            <button onClick={() => store.login(email, password)}>логин</button>
+            <button onClick={() => store.registration(email, password)}>регистрация</button>
+        </div>
+    )
+}
+
+export default LoginForm
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import Store from './store/store.ts'
+
+interface State {
+  store: Store
+}
+
+const store = new Store()
+
+export const Context = createContext<State>({
+  store
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Context.Provider value={{
+    store
+  }}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+
+  </Context.Provider>
+
 )
